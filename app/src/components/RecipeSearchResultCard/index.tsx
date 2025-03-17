68d6c1c46ea73f2ae966388c2540ab842b077853
Niru Bhandari <nirubhandari45@gmail.com>
2025-03-17 21:44:54 +0545

    feat : added navigation in the searched recipe

--- app/src/components/RecipeSearchResultCard/index.tsx
+++ app/src/components/RecipeSearchResultCard/index.tsx
@@ -1,18 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const RecipeSearchResultCard = ({ recipe }: any) => {
   return (
-    <div className="p-4 border border-gray-300 rounded-lg mb-2 flex">
-      <img
-        src={recipe.recipeImageURL}
-        alt="recipe"
-        className="w-full h-48 object-cover rounded-lg mt-2"
-      />
-      <div>
-        <h3 className="text-lg font-semibold">{recipe.recipeTitle}</h3>
-        <p className="text-gray-600">{recipe.description}</p>
+    <Link href={`/recipe/${recipe.recipeId}`}>
+      <div className="p-5 border  border-gray-300 rounded-lg mb-2 grid grid-cols-2 w-[100%] gap-12 items-center">
+        <div>
+          <img
+            src={recipe.recipeImageURL}
+            alt="recipe"
+            className=" hover:scale-110 w-full h-24 object-cover rounded-lg mt-2"
+          />
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold">{recipe.recipeTitle}</h3>
+          <p className="text-gray-600">{recipe.recipeDescription}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- app/src/components/searchrecipe/index.tsx
+++ app/src/components/searchrecipe/index.tsx
@@ -22,21 +22,23 @@ const SearchRecipeCard = () => {
   return (
     <div className="flex flex-col justify-center w-full items-center">
       <SearchBox
-        className="max-w-5xl"
+        className="max-w-2xl"
         onChange={handleInputChange}
         inputValue={searchQuery}
       />
 
-      <div className="mt-4">
+      <div className="mt-4 max-w-2xl relative w-full flex  flex-col max-h-[500px] overflow-scroll border rounded-[20px] p-3 border-[#cdcbcb]">
         {filteredRecipeCollection.length > 0
           ? filteredRecipeCollection.map((recipe: any, index) => (
-              <div key={index}>
+              <div key={index} className="">
                 <RecipeSearchResultCard recipe={recipe} />
               </div>
             ))
           : searchQuery && (
               <p className="text-gray-500 mt-4">No recipes found.</p>
             )}
+        {/* White Gradient at the Bottom */}
+        <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-white to-transparent pointer-events-none" />
       </div>
     </div>
   );
